dde2f8bd305752b5eee3e5bbb234e0671415545e
Josh <joshuamyers1097@gmail.com>
2025-06-12 00:14:36 +0100

    Rough plan for modules of architecture

---
+++ model.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn as nn
+import sklearn
+import pandas as pd
+import numpy as np
+
+"""
+
+I plan to implement the Band Split Rope Transformer from the following paper https://arxiv.org/pdf/2309.02612
+
+"""
+
+# stft
+
+# band split module
+
+# RoPE transformer blocks
+
+# multi-band mask estimation
+
+# istft
